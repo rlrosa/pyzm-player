6af02cc83a05bd51d90d26b6facb9b37fd9b097c
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2013-01-04 13:15:10 -0200

    [add] client port configurable

--- zmq-client.py
+++ zmq-client.py
@@ -10,13 +10,15 @@ class PyzmClient:
     """Simple command-line to zmq client for pyzm-player"""
     context = None
     sender  = None
+    server  = None
     port    = None
 
-    def __init__(self, port):
+    def __init__(self, server, port):
+        self.server  = server
         self.port    = port
         self.context = zmq.Context()
         self.sender  = self.context.socket(zmq.REQ)
-        self.sender.connect('tcp://*:%d' % port)
+        self.sender.connect('tcp://%s:%d' % (server,port))
 
     def __enter__(self):
         return self
@@ -49,18 +51,20 @@ def main(argv):
     def usage():
         help_str = "Valid arguments:\n"\
             "\t-p portNumber\t//\t--port=portNumber\n"\
+            "\t-s serverIP\t//\t--server=xxx.xxx.xxx.xxx\n"\
             "\t-i\t\t//\t--info\n"
         sys.stderr.write("%s" % help_str)
         sys.exit(1)
 
     # default values
     port      = 5555
+    server    = '*'
     file_name = None
 
     # parse command line arguments
     arg_list = argv[1:]
     try:
-        opts, args = getopt.getopt(arg_list, "ip:", ["info", "port="])
+        opts, args = getopt.getopt(arg_list, "ip:s:", ["info", "port=", "server="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -70,8 +74,10 @@ def main(argv):
             sys.exit()
         elif opt in ("-p", "--port"):
             port = int(arg)
+        elif opt in ("-s", "--server"):
+            server = arg
 
-    client = PyzmClient(port)
+    client = PyzmClient(server,port)
     client.run()
 
 if __name__ == '__main__':
