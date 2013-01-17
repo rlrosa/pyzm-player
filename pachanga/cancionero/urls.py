c5838f6716a2d60fd57a17dd7ae93c1278c3ae96
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2013-01-17 19:59:23 -0200

    [fix] rm extra '/'

--- pachanga/cancionero/urls.py
+++ pachanga/cancionero/urls.py
@@ -4,16 +4,14 @@ from cancionero import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^/addSong$', views.addSong,  name='addSong'),
-    url(r'^/addToPlayList$', views.addToPlayList,  name='addToPlayList'),
-    url(r'^/play$', views.play,  name='play'),
-    url(r'^/stop$', views.stop,  name='stop'),
-    url(r'^/nextSong', views.nextSong,  name='nextSong'),
-    url(r'^/prev', views.prev,  name='prev'),
-    url(r'^/dbToPlayList', views.dbToPlayList,  name='dbToPlayList'),
-    url(r'^/queueClear', views.queueClear,  name='queueClear'),
-    url(r'^/removeFromPL', views.removeFromPL,  name='removeFromPL'),
+    url(r'^addSong$', views.addSong,  name='addSong'),
+    url(r'^addToPlayList$', views.addToPlayList,  name='addToPlayList'),
+    url(r'^play$', views.play,  name='play'),
+    url(r'^stop$', views.stop,  name='stop'),
+    url(r'^nextSong', views.nextSong,  name='nextSong'),
+    url(r'^prev', views.prev,  name='prev'),
+    url(r'^dbToPlayList', views.dbToPlayList,  name='dbToPlayList'),
+    url(r'^queueClear', views.queueClear,  name='queueClear'),
+    url(r'^removeFromPL', views.removeFromPL,  name='removeFromPL'),
+    url(r'^addToDb', views.addToDb,  name='addToDb'),
 )
-
-
-  
