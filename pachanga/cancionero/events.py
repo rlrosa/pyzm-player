fca7b39df7831679dde2883dfd26ee9e1f6229ba
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2013-01-22 11:16:00 -0200

    [add] use websocket+zmq to trigger page update, still uses get (but without setInterval)

--- pachanga/cancionero/events.py
+++ pachanga/cancionero/events.py
@@ -4,13 +4,45 @@ from django_socketio import events
 
 import logging
 
+from gevent_zeromq import zmq
+from gevent import spawn
+zmq_ctx = zmq.Context()
+zmq_sub = None
+
+def zmq_subscriber(socketio):
+    # For too many threads spawning new connection will cause a
+    # "too many mailboxes" error, but for small amounts of
+    # threads this is fine.
+
+    global zmq_ctx, zmq_sub
+    subscriber = zmq_ctx.socket(zmq.SUB)
+    subscriber.connect("tcp://127.0.0.1:5556")
+
+    # setsockopt doesn't like unicode
+    subscriber.setsockopt(zmq.SUBSCRIBE, '')
+
+    socketio.send({'msg':'django connected'})
+
+    while True:
+        msg = subscriber.recv()
+        logging.debug('RX: %s' % msg)
+        if msg:
+            socketio.send({'msg':msg})
+
 @events.on_connect
 def message(request, socket, context):
     """
-    Event handler for a room receiving a message.
+    Event handler for connection establishment.
     """
-    logging.debug('Connection established!')
-    # connect to zmq, etc
+    logging.debug('websocket Connection established!')
+    # connect to zmq
+    try:
+        zmq_running = context['socket_up']
+    except KeyError as e:
+        socketio = request.environ['socketio']
+        spawn(zmq_subscriber, socketio)
+        context['socket_up'] = True
+        logging.info('zmq subscriber running')
 
 @events.on_message(channel='sucket')
 def message(request, socket, context, message):
@@ -28,3 +60,9 @@ def finish(request, socket, context):
     the user leaving and delete them from the DB.
     """
     logging.debug('leaving...')
+    if zmq_ctx:
+        if zmq_sub:
+            logging.debug('closing zmq subscriber')
+            zmq_sub.close()
+        logging.debug('terminating zmq context')
+        zmq_ctx.term()
