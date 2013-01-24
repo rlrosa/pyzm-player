c2db9705b8052d7f060cd6bd5ccdfb746e7df750
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2013-01-24 12:56:34 -0200

    [fix] run zmq subscriberwhen django starts instead of on connect
    list of sockets is handled to know when ws broadcast should be performed

--- pachanga/cancionero/events.py
+++ pachanga/cancionero/events.py
@@ -11,9 +11,8 @@ import inspect
 
 zmq_ctx = zmq.Context()
 run     = False
-zmq_t   = None
 
-def zmq_subscriber(socketio, ip='127.0.0.1', port=5556):
+def zmq_subscriber(ip='127.0.0.1', port=5556):
     # For too many threads spawning new connection will cause a
     # "too many mailboxes" error, but for small amounts of
     # threads this is fine.
@@ -25,40 +24,62 @@ def zmq_subscriber(socketio, ip='127.0.0.1', port=5556):
     try:
         # setsockopt doesn't like unicode
         zmq_sub.setsockopt(zmq.SUBSCRIBE, '')
-        socketio.send({'msg':'django connected'})
     except Exception as e:
         raise e
 
     while run:
         msg = zmq_sub.recv()
         logging.debug('%s-RX: %s' % (inspect.stack()[0][3],msg))
-        if msg:
-            socketio.send({'msg':msg})
+        if msg and sock_list:
+            sock_list[0].send_and_broadcast_channel({'msg':msg})
+            # the line above does the job, this is not required
+            # for sock in sock_list:
+            #     sock.send({'msg':msg})
     if zmq_sub:
         logging.debug('closing zmq subscriber')
         zmq_sub.close()
         zmq_sub = None
 
+zmq_t   = None
+zmq_t = threading.Thread(target=zmq_subscriber)
+zmq_t.setDaemon(True)
+run = True
+zmq_t.start()
+sock_list = []
+
 @events.on_connect
-def message(request, socket, context):
+def connect(request, socket, context):
     """
-    Event handler for connection establishment.
+    Event handler for subscribe establishment.
     """
-    logging.debug('websocket Connection established!')
-    # connect to zmq
+    print 'connect'
+    context['socket_up'] = True
+
+@events.on_subscribe(channel='sucket')
+def subscribe(request, socket, context, channel):
+    """
+    Event handler for subscribe establishment.
+    """
+    global sock_list
+    logging.debug('websocket subscriber detected!')
+    sock_list.append(socket)
+    print 'subscribe', sock_list
+
+@events.on_disconnect
+def disconnect(request, socket, context):
+    global sock_list
     try:
-        zmq_running = context['socket_up']
-    except KeyError as e:
-        socketio = request.environ['socketio']
-        zmq_t = threading.Thread(target=zmq_subscriber,
-                                 args=[socketio])
-        zmq_t.setDaemon(True)
-        global run
-        run = True
-        zmq_t.start()
-#        spawn(zmq_subscriber, socketio)
-        context['socket_up'] = True
-        logging.info('zmq subscriber running')
+        sock_list.remove(socket)
+    except: pass
+    print 'disconnect',sock_list
+
+@events.on_unsubscribe(channel='sucket')
+def unsubscribe(request, socket, context, channel):
+    global sock_list
+    try:
+        sock_list.remove(socket)
+    except: pass
+    print 'unsubscribe',sock_list
 
 @events.on_message(channel='sucket')
 def message(request, socket, context, message):
@@ -67,7 +88,7 @@ def message(request, socket, context, message):
     """
     logging.debug(message)
     msg_ans = {'msg': 'server says go away'}
-    socket.send_and_broadcast_channel(msg_ans)
+    socket.send_and_broadcast_channel({'msg':msg_ans})
 
 @events.on_finish
 def finish(request, socket, context):
@@ -75,14 +96,19 @@ def finish(request, socket, context):
     Event handler for a socket session ending in a room. Broadcast
     the user leaving and delete them from the DB.
     """
-    logging.debug('leaving...')
-    global zmq_ctx, zmq_t, run
-    run = False
-    if zmq_t:
-        logging.debug('Waiting for zmq sub thread to join()...')
-        zmq_t.join()
-        logging.debug('zmq sub thread finished!')
-    if zmq_ctx:
-        logging.debug('terminating zmq context')
-        zmq_ctx.term()
-        zmq_ctx = None
+    global sock_list
+    try:
+        sock_list.remove(socket)
+    except: pass
+    print 'finish',sock_list
+    # logging.debug('leaving...')
+    # global zmq_ctx, zmq_t, run
+    # run = False
+    # if zmq_t:
+    #     logging.debug('Waiting for zmq sub thread to join()...')
+    #     zmq_t.join()
+    #     logging.debug('zmq sub thread finished!')
+    # if zmq_ctx:
+    #     logging.debug('terminating zmq context')
+    #     zmq_ctx.term()
+    #     zmq_ctx = None
